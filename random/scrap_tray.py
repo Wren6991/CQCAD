f0289c04cb02736dec23c2c73c60eb7d7b3e68d0
Luke Wren <wren6991@gmail.com>
2024-06-18 08:21:57 +0100

    Update scrap tray dimensions for K1 Max with squashball feet

--- random/scrap_tray.py
+++ random/scrap_tray.py
@@ -1,12 +1,23 @@
 import cadquery as cq
 
-depth = 18
+# Scrap tray to slide under K1 Max, for your skirts, supports, purge lines etc.
+# Printable on K1 Max. Note the max depth for the stock feet is around 18 mm. I
+# printed the squashball feet from here:
+#
+#   https://www.printables.com/model/734250-creality-k1k1-max-squashball-foot
+#
+# Which provide sufficient depth for this tray size. I recommend increasing your
+# line width in your slicer to around 1.25x your nozzle diameter to reduce print
+# time with all the flat surfaces.
+
+depth = 35
 width = 295
-length = width / 2
+length = width * 0.8
 
 wall_thickness = 3
 corner_radius = 30
-bottom_chamfer = 5
+bottom_chamfer = 10
+handle_radius = 15
 
 n_bottom_grooves = 15
 bottom_groove_depth = 0.5
@@ -23,9 +34,9 @@ result = (
     .faces("<Z")
     .tag("bottom")
     .workplane(offset=-depth)
-    .center(0, -length / 2 + 2)
-    .circle(10)
-    .extrude(8, taper=45)
+    .center(0, -length / 2 + wall_thickness)
+    .circle(handle_radius)
+    .extrude(handle_radius - wall_thickness, taper=45)
     .faces("+Z")
     .shell(-wall_thickness)
     .workplaneFromTagged("bottom")
