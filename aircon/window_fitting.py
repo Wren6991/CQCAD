b4fdf40f230d398b09382b45145ac9f6c29ee10c
Luke Wren <wren6991@gmail.com>
2024-06-20 17:56:54 +0100

    Tested version of window screw fitting

--- aircon/window_fitting.py
+++ aircon/window_fitting.py
@@ -9,35 +9,48 @@ from util import *
 # the fitting has centering features and magnets to allow a third part to be
 # clipped on, which has a threaded collar for an AC hose.
 
-plate_hole_diameter = 148 # 150 minus clearance
-plate_thickness = 4
+# All dimensions in mm.
 
-fitting_thread_pitch = 3.2
-fitting_thread_crest = 1.6
+plate_hole_diameter      = 148 # 150 minus clearance
+plate_thickness          = 4
+
+fitting_thread_pitch     = 3.2
+fitting_thread_crest     = 1.6
 fitting_thread_clearance = 0.8
-fitting_id = plate_hole_diameter - 8
-fitting_face_chamfer = 6
-fitting_fit_chamfer = 1
+fitting_id               = plate_hole_diameter - 8
+fitting_face_chamfer     = 3.5
+fitting_fit_chamfer      = 1
 
 fitting_flange_thickness = 8
-fitting_flange_width = 15
+fitting_flange_width     = 12
 
-magnet_ring_diameter = (plate_hole_diameter + fitting_id) / 2 + fitting_flange_width
-magnet_diameter = 6.6
-magnet_depth = 3.2
-magnet_count = 8
+magnet_ring_diameter     = (plate_hole_diameter + fitting_id) / 2 + fitting_flange_width
+magnet_diameter          = 6.6
+magnet_depth             = 3.2
+magnet_count             = 8
+
+grip_groove_count        = 24
+grip_groove_depth        = 0.8
+grip_groove_radius       = grip_groove_depth * 10
 
 def fitting_inner_base():
     return Feature(
-        cq.Workplane("XY")
-        .circle(fitting_flange_width + plate_hole_diameter / 2)
-        .extrude(fitting_flange_thickness)
-        .circle(plate_hole_diameter / 2)
-        .extrude(2 * fitting_flange_thickness + plate_thickness)
+        (
+            cq.Workplane("XY")
+            .circle(fitting_flange_width + plate_hole_diameter / 2)
+            .extrude(fitting_flange_thickness)
+        ).union(
+            cq.Workplane("XY")
+            .circle(plate_hole_diameter / 2)
+            .extrude(fitting_flange_thickness * 2 + plate_thickness)
+        )
         .circle(fitting_id / 2)
         .cutThruAll()
         .faces("<Z").chamfer(fitting_face_chamfer)
         .faces(">Z").chamfer(fitting_fit_chamfer)
+        .polarArray(fitting_flange_width + plate_hole_diameter / 2 + grip_groove_radius - grip_groove_depth, 0, 360, grip_groove_count)
+        .circle(grip_groove_radius)
+        .cutThruAll()
     )
 
 def fitting_outer_base():
@@ -49,6 +62,9 @@ def fitting_outer_base():
         .faces(">Z").chamfer(fitting_fit_chamfer)
         .circle(plate_hole_diameter / 2 + fitting_thread_clearance)
         .cutThruAll()
+        .polarArray(fitting_flange_width + plate_hole_diameter / 2 + grip_groove_radius - grip_groove_depth, 0, 360, grip_groove_count)
+        .circle(grip_groove_radius)
+        .cutThruAll()
     )
 
 def magnet_holes():
@@ -59,24 +75,38 @@ def magnet_holes():
         .extrude(magnet_depth)
     ).invert()
 
-def extruded_thread(pitch, crest, od, length, lefthanded=False, od_flat_fraction=0.2, id_flat_fraction=0.2):
+def thread_profile(pitch, crest, od, od_flat_fraction, id_flat_fraction):
     r0 = od / 2 + crest * 0.01
     r1 = od / 2 - crest
     z0 = pitch * od_flat_fraction / 2 
     z1 = pitch * (0.5 - id_flat_fraction / 2)
     z2 = pitch * (0.5 + id_flat_fraction / 2)
     z3 = pitch * (1 - od_flat_fraction / 2)
-    return Feature(
-        cq.Workplane("XY")
-        .polyline([
-            (r0, 0, z0),
-            (r1, 0, z1),
-            (r1, 0, z2),
-            (r0, 0, z3),
-        ]).close()
-        .twistExtrude(length - pitch, (-360 if lefthanded else 360) * (length - pitch) / pitch)
-    )
-         
+    return [
+        (r0, 0, z0),
+        (r1, 0, z1),
+        (r1, 0, z2),
+        (r0, 0, z3),
+    ]
+
+def extruded_thread(pitch, crest, od, length, lefthanded=False, od_flat_fraction=0.2, id_flat_fraction=0.2):
+    turns = (length - pitch) / pitch
+    assert(turns > 0)
+    # twistExtrude behaves strangely for large angles, so extrude one turn at a
+    # time and take a union.
+    accum = cq.Workplane("XY")
+    offset = 0
+    while turns > 0:
+        chunk = 1 if turns > 1 else turns
+        turns -= chunk
+        accum = accum.union(
+            cq.Workplane("XY")
+            .workplane(offset=offset)
+            .polyline(thread_profile(pitch, crest, od, od_flat_fraction, id_flat_fraction)).close()
+            .twistExtrude(pitch * chunk, (-360 if lefthanded else 360) * chunk)
+        )
+        offset += pitch
+    return Feature(accum)         
 
 fitting_inner = resolve_features(
     fitting_inner_base(),
@@ -84,7 +114,7 @@ fitting_inner = resolve_features(
         fitting_thread_pitch,
         fitting_thread_crest,
         plate_hole_diameter,
-        plate_thickness + fitting_flange_thickness + 3 * fitting_thread_pitch
+        plate_thickness + fitting_flange_thickness + fitting_thread_pitch
     ).translateZ(fitting_flange_thickness).invert(),
     magnet_holes()
 )
@@ -101,3 +131,7 @@ fitting_outer = resolve_features(
 
 safe_write_stl(fitting_inner, "window_fitting_inner.stl")
 safe_write_stl(fitting_outer, "window_fitting_outer.stl")
+
+if "show_object" in globals():
+    show_object(fitting_inner)
+    show_object(fitting_outer.rotate((0, 0, 0), (1, 0, 0), 180).translate((0, 0, 100)))
