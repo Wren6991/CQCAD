2f9e168754fcf3a9b648569096cba878474f5e1a
Luke Wren <wren6991@gmail.com>
2024-06-20 17:56:28 +0100

    Update watch_py script, it was looping forever when inotify-tools wasn't installed

--- scripts/watch_py.sh
+++ scripts/watch_py.sh
@@ -4,10 +4,12 @@
 # (on Ubuntu). I also recommend installing fstl as an excellent STL viewer that
 # can auto-watch files for updates.
 
+set -e
+
 while true; do
 	clear
 	echo "Watching $1 for changes. ^C to exit"
 	echo "Running..."
-	python3 $1 && echo "OK" || echo -n $'\a'
+	time python3 $1 && echo "OK" || echo -n $'\a'
 	inotifywait -qq -e close_write $1
 done
